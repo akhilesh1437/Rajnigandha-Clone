ed0cfaf363f0f748d99f26da23c7012786433be5
akhilesh1437 <akhileshsingh1437@gmail.com>
2025-01-09 22:28:57 +0530

    change scroll and scroll left

--- demo.js
+++ demo.js
@@ -1,27 +1,27 @@
-let scrollButton=document.querySelector(".scroll-show-hide")
+// let scrollButton=document.querySelector(".scroll-show-hide")
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) {
-      scrollButton.style.display = 'block';
-    } else {
-      scrollButton.style.display = 'none';
-    }
-  });
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > 300) {
+//       scrollButton.style.display = 'block';
+//     } else {
+//       scrollButton.style.display = 'none';
+//     }
+//   });
 
   
-  ScrollReveal().reveal('.betelnut-img', {
-    distance: '500px',
-    origin: 'left',
-    duration: 1500,
-    easing: 'ease-in-out',
-    reset: true
-});
+//   ScrollReveal().reveal('.betelnut-img', {
+//     distance: '500px',
+//     origin: 'left',
+//     duration: 1500,
+//     easing: 'ease-in-out',
+//     reset: true
+// });
 
-ScrollReveal().reveal('.betelnut-im2', {
-  distance: '500px',
-  origin: 'right',
-  duration: 1500,
-  easing: 'ease-in-out',
-  reset: true
-});
+// ScrollReveal().reveal('.betelnut-im2', {
+//   distance: '500px',
+//   origin: 'right',
+//   duration: 1500,
+//   easing: 'ease-in-out',
+//   reset: true
+// });
 
